52db32062ba9eb861147f82086b4f3b11659567c
JoVarHdez <papinchovargas@gmail.com>
2018-11-25 23:29:57 -0600

    Update footer.blade.php

--- VirtualShop/resources/views/layouts/footer.blade.php
+++ VirtualShop/resources/views/layouts/footer.blade.php
@@ -75,18 +75,18 @@
 						<h3 class="text-white font-weight-bold mb-3">Get in Touch</h3>
 						<ul>
 							<li class="mb-3">
-								<i class="fas fa-map-marker"></i> 123 Sebastian, USA.</li>
+								<i class="fas fa-map-marker"></i> 143 Sebastian, USA.</li>
 							<li class="mb-3">
-								<i class="fas fa-mobile"></i> 333 222 3333 </li>
+								<i class="fas fa-mobile"></i> 333 444 3333 </li>
 							<li class="mb-3">
-								<i class="fas fa-phone"></i> +222 11 4444 </li>
+								<i class="fas fa-phone"></i> +444 11 4444 </li>
 							<li class="mb-3">
 								<i class="fas fa-envelope-open"></i>
 								<a href="mailto:example@mail.com"> mail 1@example.com</a>
 							</li>
 							<li>
 								<i class="fas fa-envelope-open"></i>
-								<a href="mailto:example@mail.com"> mail 2@example.com</a>
+								<a href="mailto:example@mail.com"> mail 4@example.com</a>
 							</li>
 						</ul>
 					</div>
